d6357e545eaa49b024b3f015ce01a10aae518271
euHec <hectorsouzass@gmail.com>
2022-12-14 20:04:07 -0300

    Adicionada paleta de cores e setada que a primeira palea sempre será da cor preta

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-// capturando elementos
+// capturando elementos globais
 const body = document.querySelector('body');
 
 // funções
-
 const creath1 = () => {
   const title = document.createElement('h1');
   title.id = 'title';
@@ -11,18 +10,24 @@ const creath1 = () => {
 };
 
 const creatPallet = () => {
-  const palletColors = ['black', 'white', 'green', 'red']
+  const palletColors = ['black', 'orange', 'green', 'red']
   const sectionPalett = document.createElement('section');
-  sectionPalett.id = 'pai-colorPalette';
+  sectionPalett.id = 'color-palette';
   body.appendChild(sectionPalett);
-  const getSectionPalette = document.querySelector('#pai-colorPalette');
+  const getSectionPalette = document.querySelector('#color-palette');
   for (let index = 0; index < palletColors.length; index += 1) {
     const palett = document.createElement('div');
-    palett.id = 'color-palette';
     palett.className = 'color';
+    if (palletColors[index] !== 'white') {
+      palett.style.background = palletColors[index];
+    } else {
+      alert('Cor branca não é permitida');
+    }
     getSectionPalette.appendChild(palett);
   }
+
 };
+
 // inicialização da página
 window.onload = () => {
   creath1();
