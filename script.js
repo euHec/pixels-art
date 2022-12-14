ee0e77715157245b511a6a39f974a7be521a2dcb
euHec <hectorsouzass@gmail.com>
2022-12-14 20:26:09 -0300

    Foi adicionado um botão com os requisitos pré estabelecidos, execto pela criação de cores randomicas e alinhados os itens da página ao centro

--- script.js
+++ script.js
@@ -3,17 +3,17 @@ const body = document.querySelector('body');
 
 // funções
 const creath1 = () => {
-  const title = document.createElement('h1');
-  title.id = 'title';
-  title.innerText = 'Paleta de Cores';
-  body.appendChild(title);
+  const setH1 = document.createElement('h1');
+  setH1.id = 'title';
+  setH1.innerText = 'Paleta de Cores';
+  body.appendChild(setH1);
 };
 
 const creatPallet = () => {
   const palletColors = ['black', 'orange', 'green', 'red']
-  const sectionPalett = document.createElement('section');
-  sectionPalett.id = 'color-palette';
-  body.appendChild(sectionPalett);
+  const setSection = document.createElement('section');
+  setSection.id = 'color-palette';
+  body.appendChild(setSection);
   const getSectionPalette = document.querySelector('#color-palette');
   for (let index = 0; index < palletColors.length; index += 1) {
     const palett = document.createElement('div');
@@ -25,11 +25,21 @@ const creatPallet = () => {
     }
     getSectionPalette.appendChild(palett);
   }
+};
 
+const createbutton = () => {
+  const setbuton = document.createElement('button');
+  setbuton.id = 'button-random-color';
+  setbuton.innerText = 'Cores aleatórias';
+  setbuton.addEventListener('click', () => {
+
+  })
+  body.appendChild(setbuton);
 };
 
 // inicialização da página
 window.onload = () => {
   creath1();
+  createbutton();
   creatPallet();
 };
