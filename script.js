597f06838ede40367ac857316cef271659e826a6
euHec <hectorsouzass@gmail.com>
2022-12-14 16:42:09 -0300

    Adicionada a função que cria as div's das paletas de cores

--- script.js
+++ script.js
@@ -6,13 +6,25 @@ const body = document.querySelector('body');
 const creath1 = () => {
   const title = document.createElement('h1');
   title.id = 'title';
-  title.innerText = 'Paleta de cores';
+  title.innerText = 'Paleta de Cores';
   body.appendChild(title);
 };
 
-creath1();
+const creatPallet = () => {
+  const palletColors = ['black', 'white', 'green', 'red']
+  const sectionPalett = document.createElement('section');
+  sectionPalett.id = 'pai-colorPalette';
+  body.appendChild(sectionPalett);
+  const getSectionPalette = document.querySelector('#pai-colorPalette');
+  for (let index = 0; index < palletColors.length; index += 1) {
+    const palett = document.createElement('div');
+    palett.id = 'color-palette';
+    palett.className = 'color';
+    getSectionPalette.appendChild(palett);
+  }
+};
 // inicialização da página
-// window.onload = () => {
-//   creath1();
-// };
-
+window.onload = () => {
+  creath1();
+  creatPallet();
+};
