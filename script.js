705f29bf321391b3df303e25902d108f62a223b3
euHec <hectorsouzass@gmail.com>
2022-12-14 23:19:26 -0300

    O código foi retaforado, sedo incluso uma função dinâmica que cria elementos de acordo com os parâmetros passados e foi implementada a função que gera cores RGB aleatoria

--- script.js
+++ script.js
@@ -1,29 +1,22 @@
+// função principal
+const creatElements = (elemento, idElemento, posicao, text, classElement) => {
+  const criando = document.createElement(elemento);
+  if (text !== undefined) { criando.innerText = text; }
+  if (classElement !== undefined) { criando.className = classElement; }
+  if (criando !== undefined) { criando.id = idElemento; }
+  posicao.appendChild(criando);
+};
 // capturando elementos globais
 const body = document.querySelector('body');
+const palletColors = ['black'];
 
 // funções
-const creath1 = () => {
-  const setH1 = document.createElement('h1');
-  setH1.id = 'title';
-  setH1.innerText = 'Paleta de Cores';
-  body.appendChild(setH1);
-};
-
-const creatPallet = () => {
-  const palletColors = ['black', 'orange', 'green', 'red']
-  const setSection = document.createElement('section');
-  setSection.id = 'color-palette';
-  body.appendChild(setSection);
+const creatPallet = (turn) => {
   const getSectionPalette = document.querySelector('#color-palette');
-  for (let index = 0; index < palletColors.length; index += 1) {
-    const palett = document.createElement('div');
-    palett.className = 'color';
-    if (palletColors[index] !== 'white') {
-      palett.style.background = palletColors[index];
-    } else {
-      alert('Cor branca não é permitida');
-    }
-    getSectionPalette.appendChild(palett);
+  for (let index = 0; index < turn; index += 1) {
+    // eslint-disable-next-line sonarjs/no-use-of-empty-return-value
+    creatElements('div', '', getSectionPalette, '', 'color');
+    const getDivs = document.querySelectorAll('.color');
   }
 };
 
@@ -31,15 +24,24 @@ const createbutton = () => {
   const setbuton = document.createElement('button');
   setbuton.id = 'button-random-color';
   setbuton.innerText = 'Cores aleatórias';
-  setbuton.addEventListener('click', () => {
-
-  })
   body.appendChild(setbuton);
+  // eslint-disable-next-line no-use-before-define
+  setbuton.addEventListener('click', paintDivsColor);
 };
+
+const randomColors = () => {
+  const r = Math.round(Math.random() * 255);
+  const g = Math.round(Math.random() * 255);
+  const b = Math.round(Math.random() * 255);
+  return `rgb(${r},${g},${b})`;
+};
+
 
 // inicialização da página
 window.onload = () => {
-  creath1();
+  // elemento, idElemento, posicao, text, classElement
+  creatElements('h1', 'title', body, 'Paleta de Cores');
+  creatElements('section', 'color-palette', body);
+  creatPallet(4);
   createbutton();
-  creatPallet();
 };
