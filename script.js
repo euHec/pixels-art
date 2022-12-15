454d083d00b6c5118df611290777ba61c796d0db
euHec <hectorsouzass@gmail.com>
2022-12-15 18:51:23 -0300

    Foi feita uma refatoração do código para deixar mais fluidas as funções e foi concluída a função que pinta as divs

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 // função principal
 const creatElements = (elemento, idElemento, posicao, text, classElement) => {
   const criando = document.createElement(elemento);
-  if (text !== undefined) { criando.innerText = text; }
-  if (classElement !== undefined) { criando.className = classElement; }
-  if (criando !== undefined) { criando.id = idElemento; }
+  if (text !== undefined) {
+    criando.innerText = text;
+  }
+  if (classElement !== undefined) {
+    criando.className = classElement;
+  }
+  if (criando !== undefined) {
+    criando.id = idElemento;
+  }
   posicao.appendChild(criando);
 };
 // capturando elementos globais
 const body = document.querySelector('body');
-const palletColors = ['black'];
+// const palletColors = ['black'];
 
 // funções
 const creatPallet = (turn) => {
   const getSectionPalette = document.querySelector('#color-palette');
   for (let index = 0; index < turn; index += 1) {
-    // eslint-disable-next-line sonarjs/no-use-of-empty-return-value
-    creatElements('div', '', getSectionPalette, '', 'color');
+    // elemento, idElemento, posicao, text, classElement
+    creatElements('div', '', getSectionPalette, 'preen', 'color');
   }
 };
 
@@ -27,21 +33,34 @@ const randomColors = () => {
 };
 
 const paintDivsColor = () => {
+  const palletColors = ['black'];
+  const getDivColors = document.querySelectorAll('.color');
   for (let index = 1; index < getDivColors.length; index += 1) {
-    palletColors[index] = randomColors();
+    palletColors.push(randomColors());
   }
-  const getDivColors = document.querySelectorAll('.color')[0];
-  getDivColors.style.backgroundcolor = palletColors[0];
+  for (let index = 0; index < palletColors.length; index += 1) {
+    getDivColors[index].style.backgroundColor = palletColors[index];
+  }
 
+  return palletColors;
 };
 
+const creaPixell = (turn) => {
+  const getSectionPalette = document.querySelector('#color-pixel');
+  for (let index = 0; index < turn; index += 1) {
+    // eslint-disable-next-line sonarjs/no-use-of-empty-return-value
+    // elemento, idElemento, posicao, text, classElement
+    creatElements('div', '', getSectionPalette, '', 'pixel');
+  }
+};
 // inicialização da página
 window.onload = () => {
   // elemento, idElemento, posicao, text, classElement
   creatElements('h1', 'title', body, 'Paleta de Cores');
   creatElements('section', 'color-palette', body);
-  creatPallet(5);
+  creatPallet(4);
+  creatElements('section', 'color-pixel', body);
   creatElements('button', 'button-random-color', body, 'Cores aleatórias');
-
-  console.log(paintDivsColor());
+  creaPixell(25);
+  paintDivsColor();
 };
