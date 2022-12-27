ce5df15fd7ec411eb2aa27370c1d342a2a8d34e3
euHec <hectorsouzass@gmail.com>
2022-12-26 23:06:03 -0300

    Correção do erro diversos erros que o código apresentou

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-/* eslint-disable max-params */
 // função principal
-function creatElements(elemento, idElemento, posicao, text, classElement) {
+const creatElements = (elemento, idElemento, posicao, text, classElement) => {
   const criando = document.createElement(elemento);
   if (text !== undefined) {
     criando.innerText = text;
@@ -12,20 +11,26 @@ function creatElements(elemento, idElemento, posicao, text, classElement) {
     criando.id = idElemento;
   }
   posicao.appendChild(criando);
-}
+};
+
 // capturando elementos globais
 const body = document.querySelector('body');
 const sectionPalette = document.querySelector('#section-palette');
 const sectionButton = document.querySelector('#section-button');
 const getSectionPalette = document.querySelector('#color-pixel');
-// funções
+// const selected = document.querySelector('.selected');
+const colorPalette = localStorage.getItem('colorPalette');
+
 // Criando paleta de cores
 function creatPalette(turn) {
   for (let index = 0; index < turn; index += 1) {
-    // elemento, idElemento, posicao, text, classElement
-    creatElements('div', 'color-palette', sectionPalette, '', 'color');
+    const generetePalette = document.createElement('div');
+    generetePalette.id = 'color-palette';
+    generetePalette.className = 'color';
+    sectionPalette.appendChild(generetePalette);
   }
 }
+
 // cores randomicas
 const randomColors = () => {
   const r = Math.round(Math.random() * 255);
@@ -33,11 +38,43 @@ const randomColors = () => {
   const b = Math.round(Math.random() * 255);
   return `rgb(${r},${g},${b})`;
 };
+
 // função para guardar dados
 const saveLocalStorage = () => {
   // capturando as divs com classe color
   localStorage.setItem('colorPalette', `${sectionPalette.innerHTML}`);
 };
+
+// Função que pinta cores
+const paintDivsColor = () => {
+  const getDivColors = document.querySelectorAll('.color');
+  const palletColors = ['rgb(0,0,0)'];
+  // gerando cores aleatórias e adicionando ao array
+  for (let index = 1; index < getDivColors.length; index += 1) {
+    palletColors.push(randomColors());
+  }
+  const verify = palletColors[1];
+  // Validando as cores e caso seja repetido altera a cor repetida
+  for (let index = 2; index < palletColors.length; index += 1) {
+    if (palletColors[index] === verify) {
+      palletColors.splice(index, index, randomColors());
+    }
+  }
+  // Pintando as paletas
+  for (let index = 0; index < palletColors.length; index += 1) {
+    getDivColors[index].style.backgroundColor = palletColors[index];
+  }
+};
+
+const verify = () => {
+  if (colorPalette) {
+    sectionPalette.innerHTML = localStorage.getItem('colorPalette');
+  } else {
+    paintDivsColor();
+    saveLocalStorage();
+  }
+};
+
 // Criando botão
 const creatButton = () => {
   // elemento, idElemento, posicao, text, classElement
@@ -48,36 +85,16 @@ const creatButton = () => {
   button.addEventListener('click', paintDivsColor);
   button.addEventListener('click', saveLocalStorage);
 };
-// Função que pinta cores
-const paintDivsColor = () => {
-  const palletColors = ['rgb(0,0,0)'];
-  const getDivColors = document.querySelectorAll('.color');
-  for (let index = 1; index < getDivColors.length; index += 1) {
-    palletColors.push(randomColors());
-  }
-  const verify = palletColors[1];
-  for (let index = 2; index < palletColors.length; index += 1) {
-    if (palletColors[index] === verify) {
-      palletColors.splice(index, index, randomColors());
-    }
-  }
-  for (let index = 0; index < palletColors.length; index += 1) {
-    getDivColors[index].style.backgroundColor = palletColors[index];
-  }
-};
-// função recarregar dados
-const reloadStore = () => {
-  sectionPalette.innerHTML = localStorage.getItem('colorPalette');
-};
 
-const creatPixell = () => {
+// Função que cria pixells
+const creatPixell = (turn1, turn2) => {
   const creatDivPallet = document.createElement('div');
   creatDivPallet.className = 'pixel-board';
   getSectionPalette.appendChild(creatDivPallet);
-  for (let index = 0; index < 5; index += 1) {
+  for (let index = 0; index < turn1; index += 1) {
     const line = document.createElement('div');
     creatDivPallet.appendChild(line);
-    for (let index1 = 0; index1 < 5; index1 += 1) {
+    for (let index1 = 0; index1 < turn2; index1 += 1) {
       const colun = document.createElement('div');
       colun.className = 'pixel';
       line.appendChild(colun);
@@ -85,36 +102,40 @@ const creatPixell = () => {
   }
 };
 
-// const creatPixell = () => {
-//   // elemento, idElemento, posicao, text, classElement
-//   for (let colun = 0; colun < 5; colun += 1) {
-//     creatElements('div', 'pixel-board', getSectionPalette, '', '');
-//     for (let line = 0; line < 5; line += 1) {
-//       const divColun = document.querySelector('#pixel-board');
-//       creatElements('div', 'pixel-board', divColun, '', 'pixel');
-//     }
-//   }
-// };
+// Função para capturar cores
+const getColor = () => {
+  const getDivColors = document.querySelectorAll('.color');
+  for (let index = 0; index < getDivColors.length; index += 1) {
+    getDivColors[index].addEventListener('click', (event) => {
+      console.log(event.target.style.backgroundColor);
+      selected = event.target.style.backgroundColor;
+    });
+    // paintDivs();
+  }
+};
 
-// const paintDivs = () => {
-//   const getDivColors = document.querySelectorAll('.color');
-//   for (let index = 0; index < getDivColors.length; index += 1) {
-//     getDivColors[index].addEventListener('click', (event) => {
-//       localStorage.setItem('color', event.target.style.backgroundColor);
-//     });
-//   }
-// };
+// Função para pintar cores
+const paintDivs = () => {
+  const getPixels = document.querySelectorAll('.pixel');
+  for (let index = 0; index < getPixels.length; index += 1) {
+    getPixels.addEventListener('click', () => {
+      // getPixels = selected;
+    });
+  }
+};
 
 // inicialização da página
 window.onload = () => {
   // criando pallets
   creatPalette(4);
-  // Criando botão
+
+  // pintando paletas e salvando no localStorage
+  verify();
+
+  // inserindo botão
   creatButton();
-  // Pintando paletas
-  paintDivsColor();
-  // Criando matriz
-  creatPixell();
-  // Restaurando as cores das paletas salvas no local storage
-  reloadStore();
+
+  // inserindo pixels
+  creatPixell(5, 5);
+
 };
